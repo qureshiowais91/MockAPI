6de1771be53d43750612517352a0306996f8c549
Qureshi Owais <qureshiowais91@gmail.com>
2021-12-14 11:29:17 +0530

    middlewaree  added logger

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const cats = require("./routes/cats");
+const logger = require("./middleware/logger");
 
 dotenv.config(".env");
 const PORT = process.env.PORT;
 
 const app = express();
 
+app.use(logger);
 app.use("/api/v1/cats", cats);
 
 app.listen(PORT, () => {
