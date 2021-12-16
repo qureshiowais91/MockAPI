e860df24480531d589926d75e7733a95035bb782
Qureshi Owais <qureshiowais91@gmail.com>
2021-12-16 16:17:16 +0530

    errorHandler

--- middleware/error.js
+++ middleware/error.js
@@ -1,6 +1,12 @@
 const errorHandler = (err, req, res, next) => {
   console.log(err.stack.red);
   
+  let error  = {...er}
+
+  if(err.name==="CastError"){
+  const message = `Cat not Found ${err.value}`;
+
+  }
   
   res.status(err.statusCode|| 500).json({
     success: false,
